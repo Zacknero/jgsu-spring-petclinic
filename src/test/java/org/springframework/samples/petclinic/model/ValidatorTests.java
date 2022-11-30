3cbb78e045d0165ea411056381d72d6f7ef3360d
Zack <nerodevilman@live.it>
2022-11-30 11:51:37 +0100

    Update ValidatorTests.java
    test2

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("nomepty");
+		person.setFirstName("nomepty11");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
